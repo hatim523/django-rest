ef90120f73aacc77fb3105c44f0671ca2fd46a1a
Hatim <hatimjuzer78652@gmail.com>
2020-10-02 07:28:52 +0500

    Initial commit

---
+++ languages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+app_name = "languages"
+
+router = routers.DefaultRouter()
+router.register('languages', views.LanguageView)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
